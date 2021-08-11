8cb724f80f830047d2e6ab26350225d4a03ccc59
robakerson <robakerson@gmail.com>
2021-08-10 23:45:08 -0500

    Done!

--- Band Name Generator/BandNameGenerator.py
+++ Band Name Generator/BandNameGenerator.py
@@ -1,3 +1,10 @@
-print('hello World')
 
-print('hello world')
+#Welcome and prompt user to input city, then prompt user to input a pet name
+print("Welcome to Bob's band name generator!")
+print("What is the name of the city you grew up in?")
+user_city = input()
+print("What is the name of your favorite pet?")
+user_pet_name= input()
+
+#return result of user's inputted values as their "band name"
+print("Your band name is " + user_city +" "+ user_pet_name)
