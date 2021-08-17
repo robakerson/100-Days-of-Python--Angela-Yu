cfae5fffab12b988e3e903c98c09101e36f230fa
robakerson <robakerson@gmail.com>
2021-08-17 13:46:48 -0500

    jump hurdles

--- Maze Solver (functions)/Escape Maze.py
+++ Maze Solver (functions)/Escape Maze.py
@@ -1,4 +1,25 @@
 
+#
+# # robot solve hurdle problem given unknown hurdle locations
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# # check if we are at goal
+# while at_goal()==False:
+#     # check if there is a wall in front of us. If yes, jump. If not, move
+#     if front_is_clear() == True:
+#         move()
+#     else:
+#         jump()
 
 # # define and call a function
 # def my_function():
