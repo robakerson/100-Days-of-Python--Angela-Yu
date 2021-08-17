df9f679bb22857b23dcc15cb3fab9b16603985e7
robakerson <robakerson@gmail.com>
2021-08-17 14:33:56 -0500

    variable hurdle height jump program

--- Maze Solver (functions)/Escape Maze.py
+++ Maze Solver (functions)/Escape Maze.py
@@ -1,4 +1,36 @@
 
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# #jump variable number of  hurdles of variable height with variable distance between them
+# def jump():
+#     times_went_up = 1
+#     turn_left()
+#     move()
+#     while right_is_clear() == False:
+#         move()
+#         times_went_up +=1
+#     turn_right()
+#     move()
+#     turn_right()
+#     while times_went_up > 0:
+#         times_went_up-=1
+#         move()
+#     turn_left()
+#
+# # if not at goal, check ahead, move if clear, otherwise jump
+# while at_goal()==False:
+#     if front_is_clear() == True:
+#         move()
+#     else:
+#         jump()
+
+
+
 #
 # # robot solve hurdle problem given unknown hurdle locations
 # def turn_right():
