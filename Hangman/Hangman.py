079825ba5cad3421ed8dbd2bf8c74c83375bc35e
robakerson <robakerson@gmail.com>
2021-08-18 19:10:15 -0500

    Challenge 1

---
+++ Hangman/Hangman.py
@@ -0,0 +1,16 @@
+
+
+
+# #challenge 1 take input from user, select random word, check if user's input matches letters in that word
+# import random
+# word_list = ["aardvark", "baboon", "camel"]
+# # select random word from word_list
+# chosen_word = word_list[random.randint(0,len(word_list)-1)]
+# #take input from user and assign it to guess
+# guess = input("Guess a letter of the word:")
+# # check each letter in randomly chosen word against user guess and print whether it matches
+# for letter in chosen_word:
+#   if letter == guess:
+#     print("Correct")
+#   else:
+#     print("Wrong")
