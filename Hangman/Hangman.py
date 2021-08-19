231436bf93a4f8eabc38ee823b0467c6ce6cd14d
robakerson <robakerson@gmail.com>
2021-08-18 20:32:18 -0500

    Hangman!

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,4 +1,56 @@
 
+
+import random
+import hangman_words
+import hangman_art
+
+print(hangman_art.logo)
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+guessed_letters =[]
+for _ in range(word_length):
+    display += "_"
+print(f"{' '.join(display)}")
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess not in guessed_letters:
+      guessed_letters += guess
+      #Check guessed letter
+      for position in range(word_length):
+          letter = chosen_word[position]
+          # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+          if letter == guess:
+              display[position] = letter
+      #Check if user is wrong.
+      if guess not in chosen_word:
+          lives -= 1
+          print(f"{guess} is not in the word!")
+          if lives == 0:
+              end_of_game = True
+              print("You lose.")
+    else:
+      print(f"You've already guessed {guess}. Try something else!")
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
+
+
 # # step 3 we add a loop to allow multiple guesses
 # display = []
 # for _ in range(word_length):
