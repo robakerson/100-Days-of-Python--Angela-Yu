f9f7f83b282d73dd6605aefe3c3c9995edc89b0f
robakerson <robakerson@gmail.com>
2021-08-19 21:54:05 -0500

    add art.py and finish final challenges

--- Day 8 - Caesar Cipher/Caesar Cipher.py
+++ Day 8 - Caesar Cipher/Caesar Cipher.py
@@ -1,32 +1,82 @@
-# alphabet list will be referenced to replace letters during encryption and decryption
+# # alphabet list will be referenced to replace letters during encryption and decryption
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-#ask user for all required inputs for caesar cipher program
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
 
 #one function to rule them all
 #caesar decides to decode or encode based on direction input, then performs that function
-def caesar(plain_text, shift_amount, encrypt_or_decrypt):
-  cipher_text = ""
-  #outside "if" statement takes care of bad "decrypt" inputs
-  if encrypt_or_decrypt == "encode" or encrypt_or_decrypt =="decode":
-    for letter in plain_text:
-      position = alphabet.index(letter)
-      if encrypt_or_decrypt == "encode":
-        new_position = position + shift_amount
-      else:
-        new_position = position - shift_amount
-      cipher_text += alphabet[new_position]
-    print(f"The {direction}d text is {cipher_text}")
-  else:
-    print("I can't figure out if you want to encode or decode. Try again")
+# this is Angela's implementation of the caesar cypher. I prefer the if/else method used below
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  #cool idea but only saves 2 lines and less readable than my implementation
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  #print the result
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-#call caesar function
-caesar(plain_text=text, shift_amount=shift, encrypt_or_decrypt = direction)
+
+# print cool caesar cypher logo
+from art import logo
+print(logo)
+
+# dump it all in a loop so we can prompt user to rerun the program
+execute_again = True
+while execute_again:
+#prompt user for all needed inputs
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  # we mod the shift value by 26 to ensure user doesn't throw us outside the index of the alphabet list
+  shift = shift % 26
+  #actually call caesar program given user inputs
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  #everything below to control user restarting and handle unrecognized inputs
+  go_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.")
+  if go_again=="no":
+    print("Thank you for trying my caesar cypher program!")
+    execute_again = False
+  elif go_again =="yes":
+    print("Restarting now!")
+  else:
+    print("I can't tell if you meant to restart, so I am closing the program now.")
+    execute_again = False
+
+
+
+# # alphabet list will be referenced to replace letters during encryption and decryption
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#
+# #ask user for all required inputs for caesar cipher program
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+#
+# #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+#
+# #one function to rule them all
+# #caesar decides to decode or encode based on direction input, then performs that function
+# def caesar(plain_text, shift_amount, encrypt_or_decrypt):
+#   cipher_text = ""
+#   #outside "if" statement takes care of bad "decrypt" inputs
+#   if encrypt_or_decrypt == "encode" or encrypt_or_decrypt =="decode":
+#     for letter in plain_text:
+#       position = alphabet.index(letter)
+#       if encrypt_or_decrypt == "encode":
+#         new_position = position + shift_amount
+#       else:
+#         new_position = position - shift_amount
+#       cipher_text += alphabet[new_position]
+#     print(f"The {direction}d text is {cipher_text}")
+#   else:
+#     print("I can't figure out if you want to encode or decode. Try again")
+#
+# #call caesar function
+# caesar(plain_text=text, shift_amount=shift, encrypt_or_decrypt = direction)
 
 
 
