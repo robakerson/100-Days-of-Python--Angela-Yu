bb50a869307dd9e74bc467dbe39bdf9546587006
robakerson <robakerson@gmail.com>
2021-08-19 20:29:25 -0500

    encrypt function

--- Day 8 - Caesar Cipher/Caesar Cipher.py
+++ Day 8 - Caesar Cipher/Caesar Cipher.py
@@ -1,4 +1,24 @@
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#caesar cipher encrypt function
+def encrypt(text, shift):
+  encrypted_ans = ""
+  for letter in range(len(text)):
+    shift_index = (alphabet.index(text[letter]) + shift)
+    if shift_index > 25:
+      shift_index -= 26
+    encrypted_ans += alphabet[shift_index]
+  print(encrypted_ans)
+
+
+encrypt(text,shift)
+
+
 # # prime number checker
 # def prime_checker(number):
 #   its_prime="bad"
