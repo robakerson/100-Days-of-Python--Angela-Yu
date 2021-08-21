4bff038be43f32530ad6fe3f83b29dc991ea0827
robakerson <robakerson@gmail.com>
2021-08-21 18:16:16 -0500

    dictionary stuff

---
+++ Day 9 - Secret Auction/Secret Auction.py
@@ -0,0 +1,26 @@
+
+
+
+# ## dictionary stuff below ##
+# #declare dictionary
+# programming_dictionary = {
+#   "Bug": "An error in a program that prevents the program from running as expected.",
+#   "Function": "A piece of code that you can easily call over and over again.",
+#   "Loop": "The action of doing something over and over again.",
+# }
+# #retrieving items from dictionary
+# print(programming_dictionary["Bug"])
+# #Adding new items to dictionary
+# programming_dictionary["Loop"]= "The action of doing something over and over again."
+# #declare empty dictionary
+# empty_dictionary = {}
+# #wipe existing dictionary
+# programming_dictionary = {}
+# #edit an item in a dictionary
+# programming_dictionary["Bug"] = "A moth in your computer"
+# #loop through a dictionary
+# for key in programming_dictionary:
+#   #print keys only
+#   print(key)
+#   #print the values
+#   print(programming_dictionary[key])
