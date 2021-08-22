f93b7214f9433cb90ff76a76b0f711d57a81c449
robakerson <robakerson@gmail.com>
2021-08-21 19:08:26 -0500

    nesting dictionaries and lists within each othrer

--- Day 9 - Secret Auction/Secret Auction.py
+++ Day 9 - Secret Auction/Secret Auction.py
@@ -1,7 +1,52 @@
 
 
 
-# ## dictionary stuff below ##
+# travel_log= {
+#   #dictionary in a dictionary
+#   "France": {"cities_visited": ["Paris", "Lille", "Dijon",]},
+#   "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+# }
+# print(travel_log["France"]["cities_visited"][0])
+
+# #create list with 2 dictionary elements, each dictionary has 3 entries, one of which is a list...
+# travel_log = [
+#   {"country": "France",
+#   "cities_visited": ["Paris", "Lille", "Dijon"],
+#   "total_visits": 12}
+#   {"country": "Germany",
+#   "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#   "total_visits": 5}
+# ]
+# print(travel_log[0]["country"])
+
+
+
+# # declare student_scores
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99,
+#   "Draco": 74,
+#   "Neville": 62,
+# }
+# #init student_grades as empty dictionary
+# student_grades = {}
+# # loops once for each entry in student_scores dictionary. key = "Harry", "Ron", etc
+# for key in student_scores:
+#     #case/switch for student scores adds KVP for "Student":"Outstanding"/"Exceeds Expectations"/etc to student_grades dictionary
+#   if student_scores[key] > 90:
+#     student_grades[key] = "Outstanding"
+#   elif student_scores[key] > 80 and student_scores[key] <91:
+#     student_grades[key] = "Exceeds Expectations"
+#   elif student_scores[key] > 70 and student_scores[key] <81:
+#     student_grades[key] = "Acceptable"
+#   elif student_scores[key] <= 70:
+#     student_grades[key] = "Fail"
+#
+# print(student_grades)
+
+
+# ## dictionary 101 stuff below ##
 # #declare dictionary
 # programming_dictionary = {
 #   "Bug": "An error in a program that prevents the program from running as expected.",
