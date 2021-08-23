e6a096ec2fa3260942bced0e4311d6b632f29665
robakerson <robakerson@gmail.com>
2021-08-22 23:50:35 -0500

    Full calulator v0.9

--- Day 10 - Calculator/Calculator.py
+++ Day 10 - Calculator/Calculator.py
@@ -1,6 +1,5 @@
 
-
-#Calculator
+#Full Calculator
 #Add
 def add(n1, n2):
   return n1 + n2
@@ -13,22 +12,72 @@ def multiply(n1, n2):
 #divide
 def divide(n1, n2):
   return n1 / n2
- #dictionary holds the operations we will use. user input is the key, value determines the function to call from above
 operations = {
   "+": add,
   "-": subtract,
   "*": multiply,
   "/": divide
 }
-#user inputs number, operation, number2
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-  print(symbol)
-operation_symbol = input("Pick an operation from the line above:" )
-num2 = int(input("What's the second number?: "))
-# call one of the 4 operation functions based on which user input is given and call that function with number inputs
-answer= operations[operation_symbol](num1,num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+
+def calculator():
+  num1 = int(input("What's the first number?: "))
+  another_operation = True
+  #if user wants to keep going, we ask for another operation and number and operate on previous result
+  while another_operation:
+    for symbol in operations:
+      print(symbol)
+    operation_symbol = input("Pick an operation from the line above:" )
+    num2 = int(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer= calculation_function(num1,num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    continue_prompt = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.:")
+    if continue_prompt == 'y':
+      num1 = answer
+    else:
+      another_operation = False
+      print(f"Your final result is {answer}")
+      #The calculator calls itself. WE NEVER STOP
+      calculator()
+
+calculator()
+
+
+
+
+
+
+
+# #First Calculator - one calculation
+# #Add
+# def add(n1, n2):
+#   return n1 + n2
+# #subtract
+# def subtract(n1, n2):
+#   return n1 - n2
+# #multiply
+# def multiply(n1, n2):
+#   return n1 * n2
+# #divide
+# def divide(n1, n2):
+#   return n1 / n2
+#  #dictionary holds the operations we will use. user input is the key, value determines the function to call from above
+# operations = {
+#   "+": add,
+#   "-": subtract,
+#   "*": multiply,
+#   "/": divide
+# }
+# #user inputs number, operation, number2
+# num1 = int(input("What's the first number?: "))
+# for symbol in operations:
+#   print(symbol)
+# operation_symbol = input("Pick an operation from the line above:" )
+# num2 = int(input("What's the second number?: "))
+# # call one of the 4 operation functions based on which user input is given and call that function with number inputs
+# answer= operations[operation_symbol](num1,num2)
+# print(f"{num1} {operation_symbol} {num2} = {answer}")
 
 # #leap year function
 # def is_leap(year):
