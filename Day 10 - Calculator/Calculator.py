647754b84d7282180e999927d3b0acb298dcc6b8
robakerson <robakerson@gmail.com>
2021-08-22 22:56:29 -0500

    days in month function

--- Day 10 - Calculator/Calculator.py
+++ Day 10 - Calculator/Calculator.py
@@ -1,11 +1,35 @@
 
+# #leap year function
+# def is_leap(year):
+#   if year % 4 == 0:
+#     if year % 100 == 0:
+#       if year % 400 == 0:
+#         return True
+#       else:
+#         return False
+#     else:
+#       return True
+#   else:
+#     return False
+#
+# #days_in_month function takes year,month and returns number of days
+# def days_in_month(year, month):
+#   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#   if is_leap(year) and int(month) == 2:
+#     return 29
+#   else:
+#     return month_days[month - 1]
+#
+# #call above functions with user input
+# year = int(input("Enter a year: "))
+# month = int(input("Enter a month: "))
+# days = days_in_month(year, month)
+# print(days)
 
-
-
-# function with multiple inputs, one output
-def format_name(f_name, l_name):
-  formatted_f_name = f_name.title()
-  formatted_l_name = l_name.title()
-  return f"{formatted_f_name} {formatted_l_name}"
-# print the output of the format_name function
-print(format_name("anGelA", "yU"))
+# # function with multiple inputs, one output
+# def format_name(f_name, l_name):
+#   formatted_f_name = f_name.title()
+#   formatted_l_name = l_name.title()
+#   return f"{formatted_f_name} {formatted_l_name}"
+# # print the output of the format_name function
+# print(format_name("anGelA", "yU"))
