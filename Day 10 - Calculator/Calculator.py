51c38c9afecfdf81fbd56b14e5dda6ec10bb0a2a
robakerson <robakerson@gmail.com>
2021-08-22 22:34:10 -0500

    format name function

---
+++ Day 10 - Calculator/Calculator.py
@@ -0,0 +1,11 @@
+
+
+
+
+# function with multiple inputs, one output
+def format_name(f_name, l_name):
+  formatted_f_name = f_name.title()
+  formatted_l_name = l_name.title()
+  return f"{formatted_f_name} {formatted_l_name}"
+# print the output of the format_name function
+print(format_name("anGelA", "yU"))
