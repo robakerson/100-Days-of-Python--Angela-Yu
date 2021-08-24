2e78add95bd821ef87f0d420c4200688010da3f9
robakerson <robakerson@gmail.com>
2021-08-23 20:51:42 -0500

    blackjack v0

--- Day 10 - Calculator/Calculator.py
+++ Day 10 - Calculator/Calculator.py
@@ -21,14 +21,14 @@ operations = {
 
 
 def calculator():
-  num1 = int(input("What's the first number?: "))
+  num1 = float(input("What's the first number?: "))
   another_operation = True
   #if user wants to keep going, we ask for another operation and number and operate on previous result
   while another_operation:
     for symbol in operations:
       print(symbol)
     operation_symbol = input("Pick an operation from the line above:" )
-    num2 = int(input("What's the next number?: "))
+    num2 = float(input("What's the next number?: "))
     calculation_function = operations[operation_symbol]
     answer= calculation_function(num1,num2)
     print(f"{num1} {operation_symbol} {num2} = {answer}")
