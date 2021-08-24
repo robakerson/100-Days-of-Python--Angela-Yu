ce36339095f7aa791b52a00dd0c6a1e0037ae63f
robakerson <robakerson@gmail.com>
2021-08-23 23:09:00 -0500

    blackjack!

--- Day 11 - Blackjack/Blackjack.py
+++ Day 11 - Blackjack/Blackjack.py
@@ -3,15 +3,18 @@ from art import logo
 import random
 from replit import clear
 
+#returns random value from cards list
 def randomcard():
   return cards[random.randint(0,12)]
 
+#deals 2 cards to player and dealer for start of game
 def deal_starting_cards():
   player_cards.append(randomcard())
   player_cards.append(randomcard())
   dealer_cards.append(randomcard())
   dealer_cards.append(randomcard())
 
+#summates value of hand, and replaces any aces with 1 if hand is bust with ace in hand
 def score_hand(hand):
   score = 0
   for cards in hand:
@@ -26,10 +29,12 @@ def score_hand(hand):
   else:
     return score
 
+#dealer pulls cards until score is over 17
 def dealer_play(hand):
   while score_hand(hand) < 17:
     hand.append(randomcard())
 
+# check if user or dealer has blackjack, we stop the game and display final score immediately if true
 def check_blackjack():
   #check if user has blackjack & win immediately
   if player_cards == [10, 11] or player_cards == [11, 10]:
@@ -48,6 +53,7 @@ def check_blackjack():
     print("Lose, opponent has Blackjack 😱")
     return True
 
+# compare user and dealer hand and generate appropriate end message
 def present_winner(player_cards, dealer_cards):
   dealer_play(dealer_cards)
   final_player_score = score_hand(player_cards)
@@ -65,10 +71,12 @@ def present_winner(player_cards, dealer_cards):
   elif final_dealer_score == final_player_score:
     print("Draw 🙃")
 
+#recursive function to allow player to pull cards until bust or satisfied
 def player_play():
   cur_player_score = score_hand(player_cards)
   print(f"   Your cards: {player_cards}, current score: {cur_player_score} ")
   print(f"   Computer's first card: {dealer_cards[0]}")
+  #if player busts, we will stop them playing more.
   if cur_player_score > 21:
     return
   else:
@@ -77,13 +85,15 @@ def player_play():
     player_cards.append(randomcard())
     player_play()
 
-
+# the actual game. Ask player to play? then call all appropriate functions in order to make the game play
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n':") == 'y':
+    #initialize the screen
   clear()
   print(logo)
   player_cards = []
   dealer_cards = []
   deal_starting_cards()
+  # if noone has blackjack, let player play then present the winner
   if not check_blackjack():
     player_play()
     present_winner(player_cards, dealer_cards)
