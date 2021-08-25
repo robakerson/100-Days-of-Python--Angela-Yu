cc7e93ca98950e74529de38b91880990e934e9db
robakerson <robakerson@gmail.com>
2021-08-24 22:17:22 -0500

    Main game function 90%, guess_answer function

--- Day 12 - Number Guessing Game/Number Guessing Game.py
+++ Day 12 - Number Guessing Game/Number Guessing Game.py
@@ -1,4 +1,5 @@
 import random
+from replit import clear
 
 logo = '''
  o-o                       o-O-o o            o   o            o            o
@@ -8,8 +9,40 @@ o   | |  | |-'  \   \        |   |  | |-'     |  \| |  | | | | |  | |-' |
  o-o  o--o o-o o-o o-o       o   o  o o-o     o   o o--o o o o o-o  o-o o   O
 '''
 
-print(logo)
+def guess_answer(real_answer):
+  guess = int(input("Make a guess:"))
+  if guess < real_answer:
+    print("Too low. \nGuess Again.")
+  elif guess > real_answer:
+    print("Too High.\nGuess Again.")
+  else:
+    print(f"You got it! The answer was {real_answer}")
+    return True
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+def play_game():
+  clear()
+  print(logo)
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100")
+
+  difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+  attempts = 0
+  answer = random.randint(1,100)
+  print(answer)
+  if difficulty == 'easy':
+    attempts = 10
+  elif difficulty == 'hard':
+    attempts = 5
+  else:
+    print("I don't understand your input. Please try again.")
+
+  while attempts>0:
+    print(f"You have {attempts} attempts remaining to guess the answer.")
+    if guess_answer(answer):
+      attempts = 0
+    else:
+      attempts -=1
+
+
+
+play_game()
