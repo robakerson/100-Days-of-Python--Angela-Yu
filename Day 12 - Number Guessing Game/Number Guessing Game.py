3536891599e3c791ac93b138de8987de6a86e2c5
robakerson <robakerson@gmail.com>
2021-08-24 21:58:41 -0500

    starting skeleton

---
+++ Day 12 - Number Guessing Game/Number Guessing Game.py
@@ -0,0 +1,15 @@
+import random
+
+logo = '''
+ o-o                       o-O-o o            o   o            o            o
+o                            |   |            |\  |            |            |
+|  -o o  o o-o o-o o-o       |   O--o o-o     | \ | o  o o-O-o O-o  o-o o-o o
+o   | |  | |-'  \   \        |   |  | |-'     |  \| |  | | | | |  | |-' |
+ o-o  o--o o-o o-o o-o       o   o  o o-o     o   o o--o o o o o-o  o-o o   O
+'''
+
+print(logo)
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
