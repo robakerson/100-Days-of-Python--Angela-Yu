23914f29548c5898611192865c2009afa5781ee3
robakerson <robakerson@gmail.com>
2021-08-29 22:57:10 -0500

    question_bank made

--- Day 17 - Quiz Game (Creating Objects)/main.py
+++ Day 17 - Quiz Game (Creating Objects)/main.py
@@ -1,26 +1,40 @@
-# new class
-class User:
-    pass
 
-    def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0  # default value
-        self.following = 0
+from question_model import Question
+from data import question_data
 
-    def follow(self, user):
-        user.followers +=1
-        self.following +=1
+question_bank = []
 
-# # defining attributes on the object instead of the class
-# user_1.id = "001"
-# user_1.username = "Bob"
+for question in question_data:
+    text = question['text']
+    answer = question['answer']
+    cur_question = Question(text, answer)
+    question_bank.append(cur_question)
 
-user_1 = User("001", "Bob")  # Have to provide values or class won't create
-user_2 = User("002", "Jack")
+# print(question_bank[0].text)
 
-user_1.follow(user_2)
-print(user_1.followers)
-print(user_1.following)
-print(user_2.followers)
-print(user_2.following)
+# # new class
+# class User:
+#     pass
+#
+#     def __init__(self, user_id, username):
+#         self.id = user_id
+#         self.username = username
+#         self.followers = 0  # default value
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers +=1
+#         self.following +=1
+#
+# # # defining attributes on the object instead of the class
+# # user_1.id = "001"
+# # user_1.username = "Bob"
+#
+# user_1 = User("001", "Bob")  # Have to provide values or class won't create
+# user_2 = User("002", "Jack")
+#
+# user_1.follow(user_2)
+# print(user_1.followers)
+# print(user_1.following)
+# print(user_2.followers)
+# print(user_2.following)
