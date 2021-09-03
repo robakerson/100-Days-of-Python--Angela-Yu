30bc431b13f28fdf138c7a8d65ffc644861cacc2
robakerson <robakerson@gmail.com>
2021-09-02 23:09:39 -0500

    Wall collision!

--- Day 21 - Snake Pt2/main.py
+++ Day 21 - Snake Pt2/main.py
@@ -32,7 +32,10 @@ while game_is_on:
         food.refresh()
         score.increase_score()
 
-
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
 
 
 screen.exitonclick()
--- Day 21 - Snake Pt2/scoreboard.py
+++ Day 21 - Snake Pt2/scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-
+ALIGNMENT = "center"
+FONT = ("courier", 24, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -11,8 +12,12 @@ class Scoreboard(Turtle):
         self.score = -1
         self.increase_score()
 
+    def game_over(self):
+        self.setpos(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
     def increase_score(self):
         self.score += 1
         self.clear()
         self.setpos(0, 260)
-        self.write(f"Score: {self.score}", align="center", font=("arial", 24, "normal"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
