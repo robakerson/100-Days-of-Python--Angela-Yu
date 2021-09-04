51fe74fe114612054924ba5dfe0bae38e7e7f8f6
robakerson <robakerson@gmail.com>
2021-09-04 15:28:21 -0500

    Player movement & reset

---
+++ Day 23 - Frogger/player.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.up()
+        self.shape("turtle")
+        self.setheading(90)
+        self.setpos(STARTING_POSITION)
+
+
+    def move_player(self):
+        new_y_cor = self.ycor() + MOVE_DISTANCE
+        self.setpos(self.xcor(), new_y_cor)
+
+    def reset_player(self):
+        self.setpos(STARTING_POSITION)
+
+    def check_finish_line(self):
+        return self.ycor() > FINISH_LINE_Y
+
