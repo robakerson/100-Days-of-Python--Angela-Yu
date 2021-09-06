8656f11a73bbf5c250a8d95f48fe674758b33cb3
robakerson <robakerson@gmail.com>
2021-09-05 22:49:00 -0500

    restructure mail merge

--- Day 24 - Mail Merge.Redo Snake/main.py
+++
@@ -1,9 +0,0 @@
-
-# # open a file, read and print contents.
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
-
-
-with open("my_file.txt", mode="w") as file:  # mode "w" to rewrite the whole file. mode "a" for append!
-    file.write("\nNew text.")
