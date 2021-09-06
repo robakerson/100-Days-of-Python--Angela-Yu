c88e9487a09d1a3a435e698bce5a0159322b2182
robakerson <robakerson@gmail.com>
2021-09-05 22:59:01 -0500

    begin mail merge

--- Day 24 - Mail Merge/main.py
+++ Day 24 - Mail Merge/main.py
@@ -6,3 +6,8 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Input/Names/invited_names.txt") as file:
+    list_of_names = file.readlines()
+
+print(list_of_names)
