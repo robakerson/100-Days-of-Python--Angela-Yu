97a6acd9abc5deb982e9f4aa1e626ebecde693f9
robakerson <robakerson@gmail.com>
2021-09-06 21:31:16 -0500

    using CSV to parse data

--- Day 25 - U.S. States Game/main.py
+++ Day 25 - U.S. States Game/main.py
@@ -1,4 +1,14 @@
 
+import csv
+
+with open("weather_data.csv") as file:
+    data = csv.reader(file)  # csv reader creates a loopable object
+    temperatures = []
+    for row in data:
+        if row[1] != 'temp':
+            temperatures.append(int(row[1]))
+    print(temperatures)
+
 # with open("weather_data.csv", "r") as file:
 #     data = file.readlines()
 #     print(data)
