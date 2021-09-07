05acf732c3e7a7c99f58c79adf6b8ce4fd71dfb5
robakerson <robakerson@gmail.com>
2021-09-06 22:33:01 -0500

    extract data from large dataset and export to small .csv

--- Day 25 - U.S. States Game/main.py
+++ Day 25 - U.S. States Game/main.py
@@ -1,6 +1,42 @@
 
 import pandas
 
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+# print(data["Primary Fur Color"])
+
+primary_fur_colors = ["Gray", "Cinnamon", "Black"]
+color_count = []
+for color in primary_fur_colors:
+    color_count.append(len(data[data["Primary Fur Color"] == color]))  # create list of count of each fur color
+
+color_dict = {
+    "Fur Color": primary_fur_colors,
+    "Color": color_count
+}
+
+final_data = pandas.DataFrame(color_dict)
+print(final_data)
+final_data.to_csv("Squirrel_count.csv")  # create csv file from our data!
+
+# color_table = []
+# for elem in range(len(primary_fur_colors)):
+#     color_table.append([primary_fur_colors[elem], color_count[elem]])  # create list of lists of [color, count]
+#
+# final_table = pandas.DataFrame(color_table)
+# print(final_table)
+
+# gray_col = data[data["Primary Fur Color"] == "Gray"]
+# cinnamon_col = data[data["Primary Fur Color"] == "Cinnamon"]
+# black_col = data[data["Primary Fur Color"] == "Black"]
+
+# print(len(gray_col))
+# print(len(cinnamon_col))
+# print(len(black_col))
+
+
+# print(data[data["Primary Fur Color"] == "Gray"])
+
 # data = pandas.read_csv("weather_data.csv")
 
 # print(data["temp"])  # print temperature column
@@ -15,13 +51,13 @@ import pandas
 # print(int(monday.temp))
 
 # create dataframe from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-
-data = pandas.DataFrame(data_dict)  # create dataframe from dictionary!
-data.to_csv("new_data.csv")  # convert data into .csv file!
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)  # create dataframe from dictionary!
+# data.to_csv("new_data.csv")  # convert data into .csv file!
 
 
 
