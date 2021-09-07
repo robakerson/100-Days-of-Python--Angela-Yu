cabc15b78e357ac8ab85fe3dbdf6e85a9bfbda1a
robakerson <robakerson@gmail.com>
2021-09-06 21:11:22 -0500

    import csv the hard way

---
+++ Day 25 - U.S. States Game/main.py
@@ -0,0 +1,10 @@
+
+# with open("weather_data.csv", "r") as file:
+#     data = file.readlines()
+#     print(data)
+#
+# final_data = []
+# for line in data:
+#     final_data.append(line.strip("\n"))
+#
+# print(final_data)
