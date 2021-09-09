9c6648e353427bc741fc120c55fbc0793003bd28
robakerson <robakerson@gmail.com>
2021-09-09 18:36:52 -0500

    output formatting

--- Day 27 - Tkinter/miles_to_km.py
+++ Day 27 - Tkinter/miles_to_km.py
@@ -2,11 +2,13 @@ import tkinter
 FONT = ("Arial", 24)
 
 def calculate_km():
-    km_num["text"] = int(num_entry.get()) * 1.6
+    result = float(num_entry.get()) * 1.609
+    km_num["text"] = "{0:.2f}".format(result)
 
 window = tkinter.Tk()
-window.minsize(width=350, height=200)
+# window.minsize(width=350, height=200)
 window.title("Convert miles to Km")
+window.config(padx=20, pady=20)
 
 miles_label = tkinter.Label(text="Miles", font=FONT)
 miles_label.grid(column=3, row=1)
@@ -20,7 +22,7 @@ equals_label.grid(column=1, row=2)
 km_num = tkinter.Label(text="0", font=FONT)
 km_num.grid(column=2, row=2)
 
-calc_button = tkinter.Button(text="Calculate", command=calculate_km)
+calc_button = tkinter.Button(text="Calculate", command=calculate_km, width=15)
 calc_button.grid(column=2, row=3)
 
 num_entry = tkinter.Entry()
