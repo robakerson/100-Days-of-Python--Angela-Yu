a7e4ee453d5dbee4e1142707666ac6a849ee7249
robakerson <robakerson@gmail.com>
2021-09-08 23:33:01 -0500

    NATO translator!

--- Day 26 - NATO alphabet/main.py
+++ Day 26 - NATO alphabet/main.py
@@ -1,27 +1,30 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+# student_data_frame = pandas.DataFrame(student_dict)
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+with open("nato_phonetic_alphabet.csv") as file:
+    NATO_phonetic = pandas.read_csv(file)
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+NATO_dict = {row.letter: row.code for (index, row) in NATO_phonetic.iterrows()}
+# print(NATO_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_word = input("Please input a word to translate into the NATO phonetic alphabet!: ")
 
+code_words = [NATO_dict[letter.upper()] for letter in user_word]
+print(code_words)
