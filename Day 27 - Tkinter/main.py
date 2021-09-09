08a2fddbaf75a9d20a8b3536350499995ed1a7e8
robakerson <robakerson@gmail.com>
2021-09-09 18:11:58 -0500

    miles_to_km v0

--- Day 27 - Tkinter/main.py
+++ Day 27 - Tkinter/main.py
@@ -7,7 +7,8 @@ window.minsize(width=600, height=600)
 
 # label
 my_label = tkinter.Label(text="I AM A LABEL", font=("Arial", 24, "bold"))
-my_label.pack(side="bottom")
+# my_label.pack(side="bottom")
+my_label.grid(column=10, row=10)
 
 my_label["text"] = "New Text"
 my_label.config(text="OLD TEXT")
@@ -15,14 +16,21 @@ my_label.config(text="OLD TEXT")
 
 # button
 def button_clicked():
-    my_label["text"] = "YOU CLICKED THE BUTTON"
-
+    my_label["text"] = input.get()
 
 button = tkinter.Button(text="Click me", command=button_clicked)
-button.pack()
+# button.pack()
+# button.place(x=10, y=10)
+button.grid(column=0, row=0)
+new_button = tkinter.Button(text="New Button")
+new_button.grid(column=15, row=0)
 
 
-
+# entry
+input = tkinter.Entry(width=20)
+# input.pack()
+input.grid(column=20, row=20)
+print(input.get())
 
 
 
