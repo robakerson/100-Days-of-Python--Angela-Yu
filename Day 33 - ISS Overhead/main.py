b32372fdaeda8f5b22844f88cb5cf26ed56cc726
robakerson <robakerson@gmail.com>
2021-09-15 21:52:16 -0500

    ISS location API &  Kanye v0

--- Day 33 - ISS Overhead/main.py
+++ Day 33 - ISS Overhead/main.py
@@ -1,1 +1,12 @@
 import requests
+
+
+res = requests.get(url="http://api.open-notify.org/iss-now.json")
+res.raise_for_status()
+
+data = res.json()
+
+longitude = data["iss_position"]['longitude']
+latitude = data["iss_position"]['latitude']
+
+print(longitude, latitude)
