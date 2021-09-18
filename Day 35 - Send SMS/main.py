b9122551bd89b7a263794833e756560a2cc8fa42
robakerson <robakerson@gmail.com>
2021-09-18 15:19:24 -0500

    check 12 hours for rain

--- Day 35 - Send SMS/main.py
+++ Day 35 - Send SMS/main.py
@@ -1,25 +1,39 @@
 import requests
+import os
+from twilio.rest import Client
 
 API_KEY = ""
 CITY = "Cedar Rapids,US"
 LAT = 42.0083
 LONG = -91.6441
-# API_CALL = f"http://api.openweathermap.org/data/2.5/weather"
-# CALL_PARAMS = {
-#     "q": CITY,
-#     "appid": API_KEY,
-# }
+
 API_ONECALL = f"https://api.openweathermap.org/data/2.5/onecall"
 ONECALL_PARAMS = {
     "lat": LAT,
     "lon": LONG,
     "appid": API_KEY,
+    "exclude": "current,minutely,daily"
 }
-
 
 res = requests.get(url=API_ONECALL, params=ONECALL_PARAMS)
 res.raise_for_status()
 data = res.json()
 
-print(res)
-print(data)
+check_qty_hours = 12
+hourly_data = data['hourly']
+
+it_will_rain = False
+for _ in range(check_qty_hours):
+    current_weather = hourly_data[_]['weather']  # will hold list of dictionaries
+    for num in range(len(current_weather)):
+        if current_weather[num]["id"] < 700:
+            it_will_rain = True
+
+# ------ Angela's solution, doesn't check for multiple condition codes?, don't see a huge improvement over mine -------#
+# weather_slice = hourly_data[:check_qty_hours]
+# for hour_data in weather_slice:
+#     if hour_data["weather"][0]["id"] < 700:
+#         it_will_rain = True
+
+if it_will_rain:
+    print("Bring an umbrella")
