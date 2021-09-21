8638d24ce24a9f407c5fbf4810b6c5a36e56350f
robakerson <robakerson@gmail.com>
2021-09-20 22:36:48 -0500

    refactor

--- Day 37 - Habit Tracker/main.py
+++ Day 37 - Habit Tracker/main.py
@@ -6,32 +6,36 @@ load_dotenv("C:/Users/Bob/Desktop/WebDev/100 Days of Python -Angela Yu/.env.txt"
 TOKEN = os.getenv("pixl_token")
 USERNAME = os.getenv("pixl_username")
 
-pixela_endpoint = 'https://pixe.la/v1/users'
-
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
+headers = {
+    "X-USER-TOKEN": TOKEN
 }
+
+# ------- Create profile ---------#
+# pixela_endpoint = 'https://pixe.la/v1/users'
+# user_params = {
+#     "token": TOKEN,
+#     "username": USERNAME,
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes",
+# }
 
 # create profile
 # res = requests.post(url=pixela_endpoint, json=user_params)
 # print(res.text)
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-graph_params = {
-    "id": "graph1",
-    "name": "Learning to Code",
-    "unit": "hours",
-    "type": "int",
-    "color": "ajisai"
-}
 
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
+# ------- Create a graph ---------#
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+# graph_params = {
+#     "id": "graph1",
+#     "name": "Learning to Code",
+#     "unit": "hours",
+#     "type": "int",
+#     "color": "ajisai"
+# }
 
-# create graph
 # res = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
 # print(res.text)
+
+
+# ------- Post a pixel ---------#
