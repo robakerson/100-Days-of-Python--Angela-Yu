323581bf1d15e2876a99bb4482f8ed0a0acffedc
robakerson <robakerson@gmail.com>
2021-09-25 17:05:49 -0500

    The joy of it working when you don't expect it to

--- Day 39 - Flight Tracker/flight_search.py
+++ Day 39 - Flight Tracker/flight_search.py
@@ -1,3 +1,7 @@
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        pass
+
+    def search_city(self, city):
+        return "TESTING"
--- Day 39 - Flight Tracker/main.py
+++ Day 39 - Flight Tracker/main.py
@@ -1,1 +1,33 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import os
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+from flight_search import FlightSearch
+
+load_dotenv("C:/Users/Bob/Desktop/WebDev/100 Days of Python -Angela Yu/.env.txt")
+SHEETY_USERNAME = os.getenv("sheety_username")
+SHEETY_AUTH = os.getenv('sheety_auth')
+
+sheety_endpoint = f"https://api.sheety.co/{SHEETY_USERNAME}/flightDeals/prices"
+sheety_header = {
+    'Authorization': SHEETY_AUTH
+}
+
+fsearch = FlightSearch()
+
+# res = requests.get(url=sheety_endpoint, headers=sheety_header)
+# res.raise_for_status()
+#
+# result = res.json()
+
+
+result = {'prices': [{'city': 'Paris', 'iataCode': '', 'lowestPrice': 54, 'id': 2}, {'city': 'Berlin', 'iataCode': '', 'lowestPrice': 42, 'id': 3}, {'city': 'Tokyo', 'iataCode': '', 'lowestPrice': 485, 'id': 4}, {'city': 'Sydney', 'iataCode': '', 'lowestPrice': 551, 'id': 5}, {'city': 'Istanbul', 'iataCode': '', 'lowestPrice': 95, 'id': 6}, {'city': 'Kuala Lumpur', 'iataCode': '', 'lowestPrice': 414, 'id': 7}, {'city': 'New York', 'iataCode': '', 'lowestPrice': 240, 'id': 8}, {'city': 'San Francisco', 'iataCode': '', 'lowestPrice': 260, 'id': 9}, {'city': 'Cape Town', 'iataCode': '', 'lowestPrice': 378, 'id': 10}]}
+sheet_data = result['prices']
+
+for place in sheet_data:
+    if not place['iataCode']:
+        place['iataCode'] = fsearch.search_city(place['city'])
+
+
+print(result)
+print(sheet_data)
