9b6c5f5fc5894244d6e8c571ad9de7b51ac24f97
robakerson <robakerson@gmail.com>
2021-09-28 20:22:08 -0500

    parse website with beautifulsoup

--- Day 45 - Webscraping Beautiful Soup/main.py
+++ Day 45 - Webscraping Beautiful Soup/main.py
@@ -1,5 +1,9 @@
+from bs4 import BeautifulSoup
 
-with open("website.html") as file:
-    contents = file.readlines()
+with open("website.html", encoding='utf-8') as file:
+    contents = file.read()
 
-print(contents)
+soup = BeautifulSoup(contents, 'html.parser')
+
+# print(soup.meta)
+# print(soup.title.text)
