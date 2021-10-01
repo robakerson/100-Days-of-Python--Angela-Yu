c44de8f04f532852df2bed3c018a0350583d97e2
robakerson <robakerson@gmail.com>
2021-09-30 19:15:43 -0500

    import spotify params

--- Day 46 - Music Time Machine/main.py
+++ Day 46 - Music Time Machine/main.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
 import requests
+load_dotenv("C:/Users/Bob/Desktop/WebDev/100 Days of Python -Angela Yu/.env.txt")
 
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-
 URL = f"https://www.billboard.com/charts/hot-100/{date}"
+CLIENT_ID = os.getenv("spotify_client_ID")
+CLIENT_SECRET = os.getenv("spotify_client_secret")
 
 res = requests.get(url=URL)
 movies_site = res.text
