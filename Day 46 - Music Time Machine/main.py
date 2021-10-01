c64006acdfa3f447b3f77378ef3a4aabffb3e36f
robakerson <robakerson@gmail.com>
2021-09-30 19:38:02 -0500

    spotify auth

--- Day 46 - Music Time Machine/main.py
+++ Day 46 - Music Time Machine/main.py
@@ -1,13 +1,29 @@
 from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
-import requests
+
 load_dotenv("C:/Users/Bob/Desktop/WebDev/100 Days of Python -Angela Yu/.env.txt")
+CLIENT_ID = os.getenv("spotify_client_ID")
+CLIENT_SECRET = os.getenv("spotify_client_secret")
 
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 URL = f"https://www.billboard.com/charts/hot-100/{date}"
-CLIENT_ID = os.getenv("spotify_client_ID")
-CLIENT_SECRET = os.getenv("spotify_client_secret")
+
+spotify = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        scope='playlist-modify-private',
+        redirect_uri='http://example.com',
+        show_dialog=True,
+        cache_path='token.txt'
+    )
+)
+
+user_id = spotify.current_user()['id']
 
 res = requests.get(url=URL)
 movies_site = res.text
