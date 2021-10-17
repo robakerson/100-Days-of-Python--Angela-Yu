1c1f9c82cca5cd30795f166cc497d7ccd9f564c4
robakerson <robakerson@gmail.com>
2021-10-16 19:27:00 -0500

    replace deprecated executable path method

--- Day 48 - Automatic game playing bot/main.py
+++ Day 48 - Automatic game playing bot/main.py
@@ -1,5 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-chrome_driver_path = "C:/Users/Bob/Desktop/WebDev/100 Days of Python -Angela Yu/chromedriver_win32/chromedriver.exe"
+# deprecated
+# chrome_driver_path = "C:/Users/Bob/Desktop/WebDev/100 Days of Python -Angela Yu/chromedriver_win32/chromedriver.exe"
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+driver.maximize_window()
+driver.get('https://www.google.com')
+
+
+# open a webpage
+driver.get("https://www.amazon.com")
+
+
+# close the browser
+# driver.close()
+driver.quit()
