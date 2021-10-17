a635f650bd1475f83dabfaf1dfc093c8d4db86eb
robakerson <robakerson@gmail.com>
2021-10-16 19:10:21 -0500

    import selenium

---
+++ Day 48 - Automatic game playing bot/main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+chrome_driver_path = "C:/Users/Bob/Desktop/WebDev/100 Days of Python -Angela Yu/chromedriver_win32/chromedriver.exe"
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
