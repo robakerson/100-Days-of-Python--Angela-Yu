fa5df4a296a7221eb49a3ca854f3c9517b87aba7
robakerson <robakerson@gmail.com>
2021-10-18 23:11:31 -0500

    open the page!

--- Day 49 - Linkedin Job application bot/main.py
+++ Day 49 - Linkedin Job application bot/main.py
@@ -1,7 +1,15 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
-import sel
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s, options=chrome_options)
+driver.maximize_window()
 
-
-
-
-# driver.get("https://www.linkedin.com/jobs/search/?currentJobId=2736585719&distance=25&f_E=1&f_JT=F%2CC%2CP%2CI%2CT&geoId=106585497&keywords=python&location=")
+driver.get("https://www.linkedin.com/jobs/search?keywords=Python&location=United%20States&locationId=&geoId=103644278&sortBy=R&f_TPR=&f_E=1&position=1&pageNum=0")
