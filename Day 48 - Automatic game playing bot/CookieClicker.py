62fad717baa026d23edc90624cefca9aed1a2698
robakerson <robakerson@gmail.com>
2021-10-18 23:08:41 -0500

    automatic linkedin v0

--- Day 48 - Automatic game playing bot/CookieClicker.py
+++ Day 48 - Automatic game playing bot/CookieClicker.py
@@ -11,7 +11,8 @@ chrome_options.add_experimental_option("detach", True)
 s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s, options=chrome_options)
 driver.maximize_window()
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+# driver.get("https://orteil.dashnet.org/cookieclicker/") # modern version
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "bigCookie")
 
 # BUILDING_PRICES = ["productPrice3","productPrice2","productPrice1","productPrice0"]
@@ -30,6 +31,9 @@ def get_cur_cookies():
 def get_cookies_per_second():
     return driver.find_element(By.ID, "cookies").text.split()[-1]
 
+# # modern version
+# def get_cur_price(product_id):
+#     return driver.find_element(By.CSS_SELECTOR, f"#{product_id} .price").text.split()[0]
 
 def get_cur_price(product_id):
     return driver.find_element(By.CSS_SELECTOR, f"#{product_id} .price").text.split()[0]
@@ -43,7 +47,7 @@ def buy_most_expensive():
         real_price = int("".join(price.split(',')))
         if cur_cookies > real_price:
             driver.find_element(By.ID, building).click()
-            buy_most_expensive()
+            # buy_most_expensive()
 
 
 game_start = time.time()
@@ -53,3 +57,4 @@ while time.time() < game_start + 300:
     buy_most_expensive()
 
 print("Cookies/second: " + get_cookies_per_second())
+
---
+++ Day 49 - Linkedin Job application bot/main.py
@@ -0,0 +1,7 @@
+
+import sel
+
+
+
+
+# driver.get("https://www.linkedin.com/jobs/search/?currentJobId=2736585719&distance=25&f_E=1&f_JT=F%2CC%2CP%2CI%2CT&geoId=106585497&keywords=python&location=")
