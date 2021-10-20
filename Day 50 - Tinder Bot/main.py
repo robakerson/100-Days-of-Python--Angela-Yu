b4e3d5adccb01d6db964e18b309b73b0adf979e3
robakerson <robakerson@gmail.com>
2021-10-20 17:38:22 -0500

    get to Facebook login screen

--- Day 50 - Tinder Bot/main.py
+++ Day 50 - Tinder Bot/main.py
@@ -5,6 +5,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv("C:/Users/Bob/Desktop/WebDev/100 Days of Python -Angela Yu/.env.txt")
+FB_LOGIN = os.getenv('facebook_login')
+FB_PASS = os.getenv('facebook_pw')
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -12,4 +19,23 @@ s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s, options=chrome_options)
 driver.maximize_window()
 
-driver.get("https://www.linkedin.com/jobs/search?keywords=Python&location=United%20States&locationId=&geoId=103644278&sortBy=R&f_TPR=&f_E=1&position=1&pageNum=0")
+driver.get("https://tinder.com/")
+
+login = driver.find_element(By.XPATH, "//*[@id='c-364499427']/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a")
+
+try:
+    accept_privacy = driver.find_element(By.XPATH, '//*[@id="c-364499427"]/div/div[2]/div/div/div[1]/button')
+    accept_privacy.click()
+except NoS:
+    pass
+
+login.click()
+
+
+time.sleep(1)
+login_FB = driver.find_element(By.XPATH, '//*[@id="c-143158991"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+login_FB.click()
+
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
