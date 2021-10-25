926c88143337d9adea42ddbf003e15daf3b4e7c5
robakerson <robakerson@gmail.com>
2021-10-24 23:03:18 -0500

    decorator function challenge

--- Day 54 - Flask Webdev/hello.py
+++ Day 54 - Flask Webdev/hello.py
@@ -14,3 +14,30 @@ def say_bye():
 
 if __name__ == '__main__':
     app.run()
+
+# Decorator functions:
+
+# def speed_calc_decorator(function):
+#     def wrapper_function():
+#         start_time = time.time()
+#         function()
+#         end_time = time.time()
+#         print(f"{function.__name__} run speed: {end_time - start_time}s")
+#
+#     return wrapper_function
+#
+#
+# @speed_calc_decorator
+# def fast_function():
+#     for i in range(10000000):
+#         i * i
+#
+#
+# @speed_calc_decorator
+# def slow_function():
+#     for i in range(100000000):
+#         i * i
+#
+#
+# fast_function()
+# slow_function()
