74a45f52098d98a389f23090bd1762f3ab04bf50
robakerson <robakerson@gmail.com>
2021-10-25 23:49:12 -0500

    make_bold decorator

--- Day 55 - HigherLower/main.py
+++ Day 55 - HigherLower/main.py
@@ -2,13 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper_function():
+        return "<b>" + function() + "</b>"
+    return wrapper_function
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style ="text-align: center">Hello, World!</h1>' \
+           '<img src="https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif" width=200>'
 
 
 @app.route('/bye')
+@make_bold
 def bye():
     return 'bye!'
 
