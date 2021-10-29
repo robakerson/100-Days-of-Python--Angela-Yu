54090616ce1cd14271961c16e2e5ac57694506f6
robakerson <robakerson@gmail.com>
2021-10-28 23:19:21 -0500

    create link with jinja

--- Day 57 - Templating/main.py
+++ Day 57 - Templating/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def home():
     year = datetime.datetime.now().year
     random_number = random.randint(1, 10)
     return render_template("index.html", num=random_number, cur_year=year)
@@ -24,6 +24,13 @@ def name(name):
     gender = res.json()['gender']
     return render_template("guess.html", name=name, age=age, gender=gender)
 
+
+@app.route('/blog')
+def get_blog():
+    blog_url = "https://api.npoint.io/ed99320662742443cc5b"
+    res = requests.get(blog_url)
+    all_posts = res.json()
+    return render_template('blog.html', posts=all_posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
