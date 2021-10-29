d7e8efb75551f9b8af20c87cce711b1fcc55b58f
robakerson <robakerson@gmail.com>
2021-10-28 22:19:10 -0500

    templating with Jinja

--- Day 57 - Templating/main.py
+++ Day 57 - Templating/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
+import datetime
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'hello!'
+    year = datetime.datetime.now().year
+    random_number = random.randint(1, 10)
+    return render_template("index.html", num=random_number, cur_year=year)
 
 
 if __name__ == '__main__':
